dab959f50f9876702e92887e3f79fbea43354221
Rob <rob@saltcreekmedia.com>
2015-11-19 18:48:05 -0800

    11-19

--- 135/SWIProlog/class.pl
+++ 135/SWIProlog/class.pl
@@ -1,9 +1,7 @@
-person(X) :- mother_of(Y,X).
-person(Y) :- mother_of(Y,X).
-mother_of(mary,carmen).
-mother_of(mary,alexander).
-mother_of(mary,andre).
+parent(carmen,herb,mary).
+parent(alexander,herb,mary).
+parent(andre,herb,mary).
 female(carmen).
 male(alexander).
 male(andre).
-sister(X,Y) :- female(X), parent(X,F,M), parent(Y,F,M).
+sister(X,Y) :- female(X), parent(X,F,M), parent(Y,F,M), X\=Y.
