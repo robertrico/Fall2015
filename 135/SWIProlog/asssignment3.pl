f01cb3eb487a48b302622d97a97d515427ec8446
Rob <rob@saltcreekmedia.com>
2015-11-29 01:54:46 -0800

    Ugh

--- 135/SWIProlog/asssignment3.pl
+++ 135/SWIProlog/asssignment3.pl
@@ -12,16 +12,18 @@ catowner(jam,sam).
 catowner(cocacola,betty).
 
 /* -------------------------------------------------------------------------
-2.  Implement the "listMins" function from Homework #2 as a Prolog rule.
-    For example, the query:  listMins([2,8,7], [5,4,4], M) should
-    result in Prolog's response:  M = [2, 4, 4].
-
-(define (listMins L M)
-	(if (null? L) '()
-		(cons (if (< (car L) (car M)) (car L)
-									  (car M)) (listMins (cdr L) (cdr M)))))
+2.  "listMins" 
 ------------------------------------------------------------------------- */
 
-listMins(X,_,[]) :- X = [] .
-listMins([J|K],[L|M],X) :- listMins(J,K,X), J < L. 
-listMins([J|K],[L|M],X) :- listMins(K,M,X), J > L. 
+listMins([],[],[]).
+listMins(L,[],L).
+listMins([],L,L).
+listMins([X|L],[Y|M],[X|H]) :- X = Y, listMins(L,M,H).
+listMins([X|L],[Y|M],[X|H]) :- X < Y, listMins(L,M,H).
+listMins([X|L],[Y|M],[Y|H]) :- X > Y, listMins(L,M,H).
+
+/* -------------------------------------------------------------------------
+5.  "overlap2" 
+------------------------------------------------------------------------- */
+
+overlap2(X,Y) :- 
