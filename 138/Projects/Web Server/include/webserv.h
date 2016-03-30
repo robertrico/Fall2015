cc6705f55ed553fbb512162dfa43acfd27c20e76
Rob <rob@saltcreekmedia.com>
2016-03-29 19:04:23 -0700

    138 WServ

---
+++ 138/Projects/Web Server/include/webserv.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <time.h> 
+#include <math.h> 
+
+#define BUFF_SIZE 2048
+#define ERR(msg){perror(msg);exit(1);}
+
+//Define a server port to access from client, and init on server
+#define SERVER_PORT 3000
+#define SERVER "localhost"
+
+struct http_request{
+	char *request_method;
+	char *http_version;
+	char *request;
+	char *content_type;
+	char *content_len;
+	char *serv;
+	char *date;
+};
+
+struct http_response{
+	int *status_code;
+	char *status;
+	char *date;
+	char *file_contents;
+	char *content_length;
+	int *content_length_int;
+	char *full_response;
+};
+
+static char* not_found_response_template = 
+	"HTTP/1.0 404 Not Found\r\n"
+	"Content-Type: text/html\r\n"
+	"Content-Length: 67\r\n"
+	"\r\n"
+	"<html><head></head><body><h3>404 File Not found.</h3></body></html>"
+	"\r\n"
+	"\r\n";
+static char *two_hundred_ok = 
+	"HTTP/1.1 200 OK"
+	"\r\n"
+	"Server: webserv/1.0"
+	"\r\n";
+
+void buildHead(char *buffer);
+void buildResponseDate();
+void buildFullResponse();
+int getFileContents();
+int responseLen();
--- 138/Projects/Web Server/src/webserv.c
+++ 138/Projects/Web Server/src/webserv.c
@@ -1,10 +1,14 @@
-#include "tcp.h" 
+#ifndef WS_H
+#define WS_H
+#include"webserv.h" 
+#endif 
 
-struct http_request header;
+struct http_request request_header;
+struct http_response response_header;
 
 int main(int argc, char *argv[])
 {
-	int sockfd, newsockfd, clilen;
+	unsigned int sockfd, newsockfd, clilen;
 	char buffer[BUFF_SIZE];
 	struct sockaddr_in server_addr, cli_addr;
 	int n;
@@ -36,21 +40,21 @@ int main(int argc, char *argv[])
 
 		buildHead((char*)&buffer);
 
-		printf("Request Method : %s\n",header.request_method);
-		//printf("%s\n",header.http_version);
+		char *response = NULL;
+		memset(&response, 0, sizeof(request_header.request)); /* Reset Memory */
+		if(getFileContents(request_header.request)){
+			buildResponseDate();
+			buildFullResponse();
+			response = response_header.full_response;
+		}else{
+			response = not_found_response_template;
+		}
 
-		/*
+		response_header.full_response = NULL;
+		printf("Response : %s\n",response);
 
-		HTTP/1.1 200 OK
-		Date: Thu, 25 Jul 2013 03:55:00 GMT
-		Server: sadaf2605-server/1.0
-		Content-Type: text/html
-		Content-Length: 40595
-
-		< contents of filename.html follows here >
-		*/
-
-		//n = write(newsockfd,new,BUFF_SIZE);
+		n = write(newsockfd,response,strlen(response));
+		close(newsockfd);
 
 		if (n < 0)
 			ERR("Error Responding");
@@ -61,30 +65,109 @@ int main(int argc, char *argv[])
 void buildHead(char *buffer){
 	char *end_str;
 	char *token = strtok_r(buffer,"\r\n",&end_str);
+	int method_set = 0;
+	int first_line = 1;
 
 	while(token != NULL){
 		/* While there are tokens in "string" */
 		char new[strlen(token)];
 		char *end_token;
+		char *end_token2;
 		int strln = strlen(token);
 		strncpy( new, token, strlen(token) );
 		new[strln]='\0';
-		char *tok2;
-		//printf("%s\n",new);
-		tok2 = strtok_r(&new," ",&end_token);
-		int method_set = 0;
+		char *tok2,*tok3;
+
+		
+		if(first_line){
+			tok2 = strtok_r(new," ",&end_token);
+			first_line = 0;
+			tok3 = NULL;
+		}else{
+			tok3 = strtok_r(new,":",&end_token2);
+			tok2 = NULL;
+		}
 		while(tok2 != NULL){
 			if(!method_set && (strcmp(tok2,"GET") == 0 || strcmp(tok2,"POST") == 0 || strcmp(tok2,"PUT") == 0 || strcmp(tok2,"DELETE") == 0)){
-				header.request_method = malloc(strlen(tok2) * sizeof(char));
-				strcpy(header.request_method, tok2);
+				request_header.request_method = malloc(strlen(tok2) * sizeof(char));
+				strcpy(request_header.request_method, tok2);
 				method_set = 1;
 			}
-			printf("%s\n", tok2);
-			tok2 = strtok_r( NULL, "\r\n",&end_token);
+
+			tok2 = strtok_r( NULL, " ",&end_token);
+			request_header.request = malloc(strlen(tok2) * sizeof(char));
+			strcpy(request_header.request, ++tok2);
+
+			tok2 = strtok_r( NULL, " ",&end_token);
+			request_header.http_version = malloc(strlen(tok2) * sizeof(char));
+			strcpy(request_header.http_version, tok2);
+			tok2 = NULL;
+		}
+		while(tok3 != NULL){
+			tok3 = strtok_r( NULL, ":",&end_token2);
 		}
 		
 		/* Get next token: */
-		//printf("%s\n",token);
 		token = strtok_r( NULL, "\r\n",&end_str);
 	}
+}
+
+void buildResponseDate(){
+	char time_buf[1000];
+	time_t now = time(0);
+	struct tm tm = *gmtime(&now);
+	strftime(time_buf,sizeof time_buf, "Date: %a %d %b %Y %H:%M:%S %Z\r\n", &tm);
+	response_header.date = malloc(sizeof(time_buf));
+	response_header.date = time_buf;
+}
+
+int getFileContents(char *filename){
+	unsigned long length;
+	FILE *fp = fopen(filename,"rb");
+	if(fp){
+		fseek(fp,0,SEEK_END);
+		length = ftell(fp);
+		fseek(fp,0,SEEK_SET);
+		response_header.file_contents = malloc(length);
+		if(response_header.file_contents){
+			fread(response_header.file_contents,1,length,fp);
+			char cl[2048];
+			strcpy(cl,"Content-Length: ");
+			char int_len[(int)floor(log10(abs(length)))+1];
+			sprintf(int_len,"%lu",length);
+			strcat(cl,int_len);
+			strcat(cl,"\r\n");
+			strcat(cl,"\0");
+			response_header.content_length = malloc(sizeof(cl)); 
+			response_header.content_length = cl; 
+			response_header.content_length_int = (int*)length; 
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void buildFullResponse(){
+	char final_form[responseLen()+100];
+	final_form[0] = '\0';
+	final_form[responseLen()+100] = '\0';
+	strcat(final_form,two_hundred_ok);
+	strcat(final_form,response_header.date);
+	strcat(final_form,response_header.content_length);
+	strcat(final_form,"\r\n");
+	strcat(final_form,response_header.file_contents);
+	strcat(final_form,"\r\n");
+	strcat(final_form,"\r\n");
+	response_header.full_response = malloc(sizeof(final_form));
+	memset(response_header.full_response, 0, sizeof(final_form)); /* Reset Memory */
+	strcpy(response_header.full_response,final_form);
+}
+
+int responseLen(){
+	int ok = strlen(two_hundred_ok);
+	int date = strlen(response_header.date);
+	int cl = strlen(response_header.content_length);
+	int cll = (int)response_header.content_length_int;
+	return ((ok+date+cl)*sizeof(char))+cll;
+
 }
